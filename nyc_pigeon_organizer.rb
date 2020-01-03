3053a1896c024a7d16a1888fd5799bd50caebd94
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 03:16:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each { |attribute, hash|
+    hash.each { |pair, names|
+      names.each{ |name|
+        # binding.pry
+        # if result has a key name has specic name add
+        if result[name] === pair[name]
+        result[name] = {attribute => [pair]}
+        end
+      }
+    }
+  }
+
+  return result
 end
