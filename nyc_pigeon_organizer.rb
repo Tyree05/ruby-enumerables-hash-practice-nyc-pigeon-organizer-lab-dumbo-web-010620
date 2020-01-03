8910c5505b599784089dbf2e6b6713c8ef24b095
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 03:25:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
       names.each{ |name|
         # binding.pry
         # if result has a key name has specic name add
-        # if result[name] === pair[name]
+        # if result[name] != pair[name]
         result[name] = {attribute => [pair]}
         # binding.pry
         # end
