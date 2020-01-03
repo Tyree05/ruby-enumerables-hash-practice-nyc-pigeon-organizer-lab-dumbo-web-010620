2387d98f9f4793acb5bc319b0c66f31771cdcbb7
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 04:12:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,18 @@ def nyc_pigeon_organizer(data)
         # binding.pry
         # if result has a key name has specic name add
         # current_name = hash[pair][name]
-        # if result[name] === attribute[hash][pair][name]
-         result[name] = {attribute => [pair]}
+        if !result.has_key?(name) #=== attribute[hash][pair][name]
+          result[name] = {}
+        end
+
+        if !result[name].has_key?(attribute)
+          result[name][attribute] = []
+        end
+
+        if !result[name][attribute].include?(pair)
+          result[name][attribute] << pair
+        end
+         #result[name] = {attribute => [pair]}
           # elsif
         # binding.pry
         # end
